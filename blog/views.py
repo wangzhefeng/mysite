097660f7f85bc0ad41845f58c427f9fefd566fa5
wangzhefeng <wangzhefengr@163.com>
2020-05-12 00:25:17 +0800

    page split

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Blog, BlogType
+
 
 # Create your views here.
 def blog_list(request):
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 10) # 每10页进行分页
+    page_num = request.GET.get("page", 1) # 获取页码参数(GET请求)
+    page_of_blogs = paginator.get_page(page_num)
+
     context = {}
-    context["blogs"] = Blog.objects.all()
+    # context["blogs"] = Blog.objects.all()
+    # context["blogs"] = page_of_blogs.object_list
     context["blog_types"] = BlogType.objects.all()
+    context["page_of_blogs"] = page_of_blogs
     context["blogs_count"] = Blog.objects.all().count()
     return render_to_response("blog/blog_list.html", context)
 
